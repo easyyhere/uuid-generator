e48f9bd15fffc53e4a439f1b3a9598aa959843ec
Piotr Gluchowski <pgluchowski18@gmail.com>
2025-04-12 23:47:47 +0200

    Dodano kolorowanie outputu za pomocą chalk

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+const chalk = require('chalk');
 
-console.log("Twoje nowe unikalne ID to:");
-console.log(uuidv4());
+console.log(chalk.green("Twoje nowe unikalne ID to:"));
+console.log(chalk.blue.bold(uuidv4()));
